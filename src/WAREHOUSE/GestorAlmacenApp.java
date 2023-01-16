d2b0f2d0c9413cee0e2a09ecb17f863689531d93
Chris2609 <megustas260904@gmail.com>
2023-01-16 09:38:54 +0100

    haciendo suma del stock, funciona pero con fallos al sobreescribir el fichero

--- src/WAREHOUSE/GestorAlmacenApp.java
+++ src/WAREHOUSE/GestorAlmacenApp.java
@@ -1,5 +1,6 @@
 package WAREHOUSE;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Iterator;
 
 import javax.swing.*;
@@ -17,34 +18,46 @@ public class GestorAlmacenApp {
 							+  "3. Ver el artículo más caro\n" + "4. Ver los articulos con stock menor que 10\n" + "5. Saber si hay stock de un producto\n" + "6. Ver articulos saludables\n" + "0. Salir\n"));
 			switch (opcion_menu) {
 			case 1:
-				JOptionPane.showMessageDialog(null, "primera opcion seleccionada");
-									
+				for(Articulo arti:cargar.articulos) {
+					
+					int eleccion = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce la cantidad a disminuir"));
+					arti.disminuirStock(opcion_menu);
+				}
 				
 				break;
 			case 2:
-				JOptionPane.showMessageDialog(null, "segunda opcion seleccionada");
+				String articuloAum = JOptionPane.showInputDialog(null, "Introduce el nombre del articulo a aumentar stock");
+				int cantidad = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce la cantidad a aumentar"));
+				for(Articulo art:cargar.articulos) {
+					if(art.getName().equals(articuloAum.toLowerCase())) {
+						art.incrementarStock(cantidad);
+					}
+				}
+				PrintWriter writer = new PrintWriter("datos/almacen.txt");
+					writer.println(cargar.articulos);
+				
+				writer.close();
+				
 				break;
+				
 			case 3:
-				JOptionPane.showMessageDialog(null, "tercera opcion seleccionada");
-				break;
-			case 4:
 			
 				JOptionPane.showMessageDialog(null, "El producto más caro es: \n" + cargar.elMasCaro());
 				break;
 				
-			case 5:
+			case 4:
 				for (int i = 0; i < cargar.stockJusto().size(); i++) {
 					JOptionPane.showMessageDialog(null, cargar.stockJusto().get(i));
 				}
 				break;
 			
-			case 6:
+			case 5:
 				String nombrepro = (JOptionPane.showInputDialog(null, "Introduce el nombre del producto"));
 				JOptionPane.showMessageDialog(null, cargar.hayStock(nombrepro));
 			
 				break;
 				
-			case 7:
+			case 6:
 				Refresco refres = new Refresco();
 				
 				for(Articulo arti : cargar.articulos) {
